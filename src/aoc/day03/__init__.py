6469eec191e6f72be84d9850e691048ee5d7648f
Björn Petersen <git@bjoernpetersen.net>
2022-12-04 19:18:31 +0100

    Implement day 3 task 1

--- src/aoc/day03/__init__.py
+++ src/aoc/day03/__init__.py
@@ -5,10 +5,21 @@ from aoc.spi import Day
 from aoc.spi import Task
 
 
+def priority(item: str) -> int:
+    base = 1 if item.islower() else 27
+    return "abcdefghijklmnopqrstuvwxyz".index(item.lower()) + base
+
+
 class Task1(Task):
     def calculate(self, input_lines: Iterable[str], working_dir: Path) -> int:
-        print("not implemented")
-        return 0
+        rucksacks: list[tuple[str, str]] = [
+            (line[: len(line) // 2], line[-len(line) // 2 :]) for line in input_lines
+        ]
+        return sum(
+            priority(item)
+            for r in rucksacks
+            for item in set.intersection(set(r[0]), set(r[1]))
+        )
 
 
 day = Day([Task1()])
